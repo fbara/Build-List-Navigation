fa45bd8e09e1b45447102aaa92a0748455072d80
Frank S. Bara <fbara1@yahoo.com>
2019-06-26 14:11:10 -0500

    Add new files including a list view.

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Frank Bara on 6/26/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList : View {
+    var body: some View {
+        
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkList_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
+#endif
